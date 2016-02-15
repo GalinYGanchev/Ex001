86bc6e0a3dd243951d4febc99db05958bc97c397
group <galin_psy@abv.bg>
2016-02-15 20:32:45 +0200

    first attempt

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world!");
+		System.out.println("Thanks for the fish");
+		System.out.println("Have a great day!!!");
+		System.out.println("....");
+		System.out.println("cool");
+	}
+
+}
